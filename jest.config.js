5bd17f0f016946d3278a3da7b59c72538f073989
ix5231 <ix5231if@gmail.com>
2022-08-09 21:26:42 +0900

    テストカバレッジを出力

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/{db,logger}.ts',
+  ],
+  coverageReporters: ['text', 'html'],
 };
